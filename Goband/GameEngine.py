02c8ad3a41189d776ecad2d09e8bc38746758958
piggrief <zhuzhibeishang@126.com>
2019-06-02 19:45:45 +0800

    Gobang Rule Success Finish

--- Goband/GameEngine.py
+++ Goband/GameEngine.py
@@ -1,3 +1,176 @@
 #!/usr/bin/env python 
 # -*- coding:utf-8 -*-
 
+
+class GobangAction:
+
+    ActionPoint_Row = 0
+    ActionPoint_Col = 0
+    ActionIndex = 0      # 0代表落白子，1代表落黑子，2代表不落子
+
+    def __init__(self, Row, Col, Index):
+        """
+        构造行动信息。
+        :param Row: 落子行
+        :param Row: 落子列
+        :Index Row: 落子操作
+        """
+        self.ActionPoint_Row = Row
+        self.ActionPoint_Col = Col
+        self.ActionIndex = Index
+        return
+
+
+class GobangRuleEngine:
+    """
+    五子棋规则引擎类
+    """
+    ChessBoardSize = 7
+
+    def __init__(self):
+        return
+
+    def CheckGameEnd(self, NowAction, ChessBoard):
+        """
+        检测游戏是否结束
+        :param NowAction: 当前行动
+        :param ChessBoard: 要被检测的棋盘，类型是列表的列表
+        """
+
+        Row = NowAction.ActionPoint_Row
+        Col = NowAction.ActionPoint_Col
+        ChessIndex = NowAction.ActionIndex
+
+        ColIndex = Col - 1
+        ChessNum = 0
+        Flag_QuitWhile = False
+        # 行左扫
+        while not Flag_QuitWhile:
+            if ColIndex < 0:
+                break
+            else:
+                if ChessBoard[Row][ColIndex] == ChessIndex:
+                    ChessNum = ChessNum + 1
+                else:
+                    Flag_QuitWhile = True
+
+                ColIndex = ColIndex - 1
+        ColIndex = Col + 1
+        Flag_QuitWhile = False
+        # 行右扫
+        while not Flag_QuitWhile:
+            if ColIndex >= self.ChessBoardSize:
+                break
+            else:
+                if ChessBoard[Row][ColIndex] == ChessIndex:
+                    ChessNum = ChessNum + 1
+                else:
+                    Flag_QuitWhile = True
+
+                ColIndex = ColIndex + 1
+
+        if ChessNum >= 4:
+            return True
+
+        RowIndex = Row - 1
+        ChessNum = 0
+        Flag_QuitWhile = False
+        # 列上扫
+        while not Flag_QuitWhile:
+            if RowIndex < 0:
+                break
+            else:
+                if ChessBoard[RowIndex][Col] == ChessIndex:
+                    ChessNum = ChessNum + 1
+                else:
+                    Flag_QuitWhile = True
+
+                RowIndex = RowIndex - 1
+        RowIndex = Row + 1
+        Flag_QuitWhile = False
+        # 列下扫
+        while not Flag_QuitWhile:
+            if RowIndex >= self.ChessBoardSize:
+                break
+            else:
+                if ChessBoard[RowIndex][Col] == ChessIndex:
+                    ChessNum = ChessNum + 1
+                else:
+                    Flag_QuitWhile = True
+
+                RowIndex = RowIndex + 1
+
+        if ChessNum >= 4:
+            return True
+
+        RowIndex = Row - 1
+        ColIndex = Col - 1
+        ChessNum = 0
+        Flag_QuitWhile = False
+        # ↖扫
+        while not Flag_QuitWhile:
+            if RowIndex < 0 or ColIndex < 0:
+                break
+            else:
+                if ChessBoard[RowIndex][ColIndex] == ChessIndex:
+                    ChessNum = ChessNum + 1
+                else:
+                    Flag_QuitWhile = True
+
+                RowIndex = RowIndex - 1
+                ColIndex = ColIndex - 1
+        RowIndex = Row + 1
+        ColIndex = Col + 1
+        Flag_QuitWhile = False
+        # ↘扫
+        while not Flag_QuitWhile:
+            if RowIndex >= self.ChessBoardSize or ColIndex >= self.ChessBoardSize:
+                break
+            else:
+                if ChessBoard[RowIndex][ColIndex] == ChessIndex:
+                    ChessNum = ChessNum + 1
+                else:
+                    Flag_QuitWhile = True
+
+                RowIndex = RowIndex + 1
+                ColIndex = RowIndex + 1
+
+        if ChessNum >= 4:
+            return True
+
+        RowIndex = Row - 1
+        ColIndex = Col + 1
+        ChessNum = 0
+        Flag_QuitWhile = False
+        # ↗扫
+        while not Flag_QuitWhile:
+            if RowIndex < 0 or ColIndex >= self.ChessBoardSize:
+                break
+            else:
+                if ChessBoard[RowIndex][ColIndex] == ChessIndex:
+                    ChessNum = ChessNum + 1
+                else:
+                    Flag_QuitWhile = True
+
+                RowIndex = RowIndex - 1
+                ColIndex = ColIndex + 1
+        RowIndex = Row + 1
+        ColIndex = Col - 1
+        Flag_QuitWhile = False
+        # ↙扫
+        while not Flag_QuitWhile:
+            if RowIndex >= self.ChessBoardSize or ColIndex < 0:
+                break
+            else:
+                if ChessBoard[RowIndex][ColIndex] == ChessIndex:
+                    ChessNum = ChessNum + 1
+                else:
+                    Flag_QuitWhile = True
+
+                RowIndex = RowIndex + 1
+                ColIndex = ColIndex - 1
+
+        if ChessNum >= 4:
+            return True
+
+        return False
--- Goband/UI.py
+++ Goband/UI.py
@@ -2,8 +2,10 @@
 # -*- coding:utf-8 -*-
 # import numpy as np
 
+import GameEngine as GE
 
-class GobandUI:
+
+class GobangUI:
 
     Enum_GridStatus = {
         0: 'WhiteChess',
@@ -30,20 +32,68 @@ class GobandUI:
             print('|')
         for i in range(0, 2 * ChessBoardNP[0].__len__() + 2):
             print('-', end='')
+        print()
         return
 
 
+def GetActionLocation(PlayerIndex, ActionStrBuff):
+    RowNum = 0
+    ColNum = 0
+    for index in range(0, ActionStrBuff.__len__()):
+        if ActionStrBuff[index] == ' ':
+            RowNum = int(ActionStrBuff[0:index])
+            ColNum = int(ActionStrBuff[index + 1:])
+            break
+
+    return GE.GobangAction(RowNum, ColNum, PlayerIndex)
+
+
+def ChessBoardAction(RuleEngine, ChessBoard, NowAction):
+    if not RuleEngine.CheckGameEnd(NowAction, ChessBoard):
+        if ChessBoard[NowAction.ActionPoint_Row][NowAction.ActionPoint_Col] == 2:
+            ChessBoard[NowAction.ActionPoint_Row][NowAction.ActionPoint_Col] = NowAction.ActionIndex
+        else:
+            return "此处已有棋子"
+    else:
+        if NowAction.ActionIndex == 0:
+            return "白子获胜"
+        else:
+            return "黑子获胜"
+    return "OK"
+
+
 def main():
-    NewUI = GobandUI()
+    NewUI = GobangUI()
+    NewRuleEngine = GE.GobangRuleEngine()
 
     CB = []
     for i in range(0, 7):
         Row = []
         for j in range(0, 7):
-            Row.append(1)
+            Row.append(2)
         CB.append(Row)
 
-    NewUI.PrintChessBoard(CB)
+    NowPlayer = "Black"
+    while True:
+        if NowPlayer == "Black":
+            print("黑子落子：")
+            PlayerIndex = 1
+        else:
+            print("白子落子：")
+            PlayerIndex = 0
+        ActionStr = input()
+        NowAction = GetActionLocation(PlayerIndex, ActionStr)
 
+        HintStr = ChessBoardAction(NewRuleEngine, CB, NowAction)
+        if HintStr != "OK":
+            print(HintStr)
+            continue
+
+        if NowPlayer == "Black":
+            NowPlayer = "White"
+        else:
+            NowPlayer = "Black"
+
+        NewUI.PrintChessBoard(CB)
 
 main()
